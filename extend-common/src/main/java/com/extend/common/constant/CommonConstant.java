9638c8afc96a173dd0e975f6c41039b9efbbc216
KevinClair <704714211@qq.com>
2020-12-19 21:14:32 +0800

    新增了常量管理类；

---
+++ extend-common/src/main/java/com/extend/common/constant/CommonConstant.java
@@ -0,0 +1,19 @@
+package com.extend.common.constant;
+
+/**
+ * 变量
+ *
+ * @author KevinClair
+ */
+public class CommonConstant {
+
+    /**
+     * language.
+     */
+    public static final String LANGUAGE = "lang";
+
+    /**
+     * langeage files location.
+     */
+    public static final String LANGUAGE_FILES_LOCATION = "i18n/messages";
+}
