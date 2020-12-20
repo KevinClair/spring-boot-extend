44ff60abedfc1ccebfc3ca950d8b4e31cd016eab
KevinClair <704714211@qq.com>
2020-12-20 15:50:03 +0800

    update ValidationUtils;

--- extend-common/src/main/java/com/extend/common/utils/ValidationUtils.java
+++ extend-common/src/main/java/com/extend/common/utils/ValidationUtils.java
@@ -4,24 +4,21 @@ import javax.validation.*;
 import java.util.Set;
 
 /**
- * @version 1.0
- * @ClassName ValidationUtils
- * @Description 参数校验工具类
- * @Author mingj
- * @Date 2019/12/19 10:10
- **/
-
+ * ValidationUtils。
+ *
+ * @author KevinClair
+ */
 public class ValidationUtils {
     private static final ValidatorFactory VFACTORY = Validation.buildDefaultValidatorFactory();
     private static final Validator VALIDATOR = VFACTORY.getValidator();
 
     /**
-    *@Description 实体类校验
-    *@Param [t]
-    *@Author mingj
-    *@Date 2019/12/22 18:06
-    *@Return void
-    **/
+     * 校验器
+     *
+     * @param t   需要校验的类
+     * @param <T> 泛型
+     * @throws ValidationException
+     */
     public static <T> void validate(T t) throws ValidationException {
         
         Set<ConstraintViolation<T>> set = VALIDATOR.validate(t);
