6ffea7a1c851d355ef9dfaabad203c68024e27a3
KevinClair <704714211@qq.com>
2020-12-20 15:47:40 +0800

    update Interceptor;

--- extend-common/src/main/java/com/extend/common/utils/Interceptor.java
+++ extend-common/src/main/java/com/extend/common/utils/Interceptor.java
@@ -5,6 +5,11 @@ import org.springframework.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
 
+/**
+ * Interceptor。
+ *
+ * @author KevinClair
+ */
 public class Interceptor implements MethodInterceptor {
 
     private MethodInterceptor methodInterceptor;
@@ -27,23 +32,22 @@ public class Interceptor implements MethodInterceptor {
     }
 
     /**
-    *@Description 前置校验
-    *@Param []
-    *@Author mingj
-    *@Date 2020/7/4 21:35
-    *@Return void
-    **/
+     * 拦截前置校验
+     *
+     * @param proxy       代理对象
+     * @param method      代理的方法
+     * @param params      方法参数
+     * @param methodProxy 方法代理
+     */
     protected void doBefore(Object proxy, Method method, Object[] params, MethodProxy methodProxy) {
 
     }
 
     /**
-    *@Description 后置校验
-    *@Param []
-    *@Author mingj
-    *@Date 2020/7/4 21:35
-    *@Return void
-    **/
+     * 拦截后置校验
+     *
+     * @param result 方法执行返回的结果
+     */
     protected void doAfter(Object result) {
 
     }
