8fbd35f342031a99138061eb9a8f757caaab5787
KevinClair <704714211@qq.com>
2020-12-20 15:49:45 +0800

    update RocketMQParamNameEnum;

--- extend-common/src/main/java/com/extend/common/constant/RocketMQParamNameEnum.java
+++ extend-common/src/main/java/com/extend/common/constant/RocketMQParamNameEnum.java
@@ -1,12 +1,10 @@
 package com.extend.common.constant;
 
 /**
- * @version 1.0
- * @ClassName RocketMQSerializeEnum
- * @Description RockMQ消息监听的name参数类型
- * @Author mingj
- * @Date 2020/7/26 22:57
- **/
+ * RocketMQParamNameEnum。
+ *
+ * @author KevinClair
+ */
 public enum RocketMQParamNameEnum {
     BODY(),
 
