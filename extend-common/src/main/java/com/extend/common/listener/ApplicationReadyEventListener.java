bee797b7d15e201a6a816154011dc8289eae9b80
KevinClair <704714211@qq.com>
2020-12-20 15:46:00 +0800

    update ApplicationReadyEventListener;

--- extend-common/src/main/java/com/extend/common/listener/ApplicationReadyEventListener.java
+++ extend-common/src/main/java/com/extend/common/listener/ApplicationReadyEventListener.java
@@ -6,12 +6,10 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 
 /**
- * @version 1.0
- * @ClassName ApplicationReadyEventListener
- * @Description 应用加载完成监听器
- * @Author mingj
- * @Date 2019/12/31 14:29
- **/
+ * ApplicationReadyEventListener。
+ *
+ * @author KevinClair
+ */
 public class ApplicationReadyEventListener implements ApplicationListener<ApplicationReadyEvent> {
 
     private static final Logger logger = LoggerFactory.getLogger(ApplicationReadyEventListener.class);
