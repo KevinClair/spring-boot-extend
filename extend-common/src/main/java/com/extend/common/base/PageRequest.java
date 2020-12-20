c5e06a468883a306bafbf80f0fb2eabf05049a9d
KevinClair <704714211@qq.com>
2020-12-20 15:48:52 +0800

    update PageRequest;

--- extend-common/src/main/java/com/extend/common/base/PageRequest.java
+++ extend-common/src/main/java/com/extend/common/base/PageRequest.java
@@ -3,25 +3,22 @@ package com.extend.common.base;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
- * @version 1.0
- * @ClassName PageRequest
- * @Description TODO描述
- * @Author mingj
- * @Date 2019/12/22 17:37
- **/
+ * PageRequest.
+ *
+ * @author KevinClair
+ */
 @Data
 public class PageRequest implements Serializable {
 
     private static final long serialVersionUID = -3248514679380897154L;
 
-    @Min(value = 0, message = "当前页不能小于0")
+    @Min(value = 1, message = "当前页不能小于1")
     private int pageNo;
 
-    @NotNull(message = "每页展示条数不能为空")
+    @Min(value = 1, message = "每页展示条数不能小于1")
     private int pageSize;
 
 }
