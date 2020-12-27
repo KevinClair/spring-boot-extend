7dbcea36356115cc909f3496e3803fe353c2c10e
KevinClair <704714211@qq.com>
2020-12-27 21:25:52 +0800

    update TranscationalManagement;

--- extend-mybatis/src/main/java/com/extend/mybatis/annotation/TransactionalManagement.java
+++ extend-mybatis/src/main/java/com/extend/mybatis/annotation/TransactionalManagement.java
@@ -3,17 +3,15 @@ package com.extend.mybatis.annotation;
 import java.lang.annotation.*;
 
 /**
- * @version 1.0
- * @ClassName TranscationalManagement
- * @Description 事务管理器
- * @Author mingj
- * @Date 2019/10/27 12:56
- **/
+ * TransactionalManagement.
+ *
+ * @author KevinClair
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented
-public @interface TranscationalManagement {
+public @interface TransactionalManagement {
 
     String[] value() default {};
 }
