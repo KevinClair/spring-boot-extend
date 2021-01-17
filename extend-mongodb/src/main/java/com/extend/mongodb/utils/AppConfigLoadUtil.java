a52b724f18e4742e0b95f2289e8a834c01a146db
KevinClair <704714211@qq.com>
2021-01-17 17:37:21 +0800

    配置信息加载工具类。

--- extend-mongodb/src/main/java/com/extend/mongodb/utils/AppConfigLoadUtil.java
+++ extend-mongodb/src/main/java/com/extend/mongodb/utils/AppConfigLoadUtil.java
@@ -6,22 +6,21 @@ import com.extend.mongodb.properties.MongoDbProperties;
 import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
 
+import java.util.Optional;
+
 /**
- * @version 1.0
- * @ClassName AppConfigLoadUtil
- * @Description 应用配置加载工具类
- * @Author mingj
- * @Date 2019/4/14 18:04
- **/
+ * AppConfigLoadUtil.
+ *
+ * @author KevinClair
+ */
 public class AppConfigLoadUtil {
 
     /**
-     * @Description 加载mongodb单数据源配置信息
-     * @Param [apolloConfig, env]
-     * @Author mingj
-     * @Date 2019/4/14 18:19
-     * @Return com.extend.mongodb.properties.MongodbProperties
-     **/
+     * 加载数据源配置信息
+     *
+     * @param env 环境信息
+     * @return {@link MongoDbProperties}
+     */
     public static MongoDbProperties loadMongoDbConfig(Environment env) {
         String name = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_NAME);
         String authenticationDatabase = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_REPOSITORIESAUTHENTICATIONDATABASE);
@@ -35,97 +34,52 @@ public class AppConfigLoadUtil {
         mongoDBProperties.setMongodbUri(mongodbUri);
 
         String repositoriesEnabled = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_REPOSITORIESENABLED);
-        if (!StringUtils.isEmpty(repositoriesEnabled)) {
-            mongoDBProperties.setRepositoriesEnabled(Boolean.valueOf(repositoriesEnabled));
-        } else {
-            mongoDBProperties.setRepositoriesEnabled(EnvironmentManager.MONGODB_DEFAULT_CONFIG_REPOSITORIESENABLED);
-        }
+        mongoDBProperties.setRepositoriesEnabled(!StringUtils.isEmpty(repositoriesEnabled) ? Boolean.valueOf(repositoriesEnabled) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_REPOSITORIESENABLED);
 
         String repositoriesLocations = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_REPOSITORIESLOCATIONS);
-        if (!StringUtils.isEmpty(repositoriesLocations)) {
-            mongoDBProperties.setRepositoriesLocations(repositoriesLocations);
-        }
+        Optional.ofNullable(repositoriesLocations).ifPresent(location -> mongoDBProperties.setRepositoriesLocations(repositoriesLocations));
 
         String connectionPreHost = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_CONNECTIONSPERHOST);
-        if (!StringUtils.isEmpty(connectionPreHost)) {
-            mongoDBProperties.setConnectionsPerHost(Integer.parseInt(connectionPreHost));
-        } else {
-            mongoDBProperties.setConnectionsPerHost(EnvironmentManager.MONGODB_DEFAULT_CONFIG_CONNECTIONSPERHOST);
-        }
+        mongoDBProperties.setConnectionsPerHost(!StringUtils.isEmpty(connectionPreHost) ? Integer.parseInt(connectionPreHost) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_CONNECTIONSPERHOST);
 
         String connectionTime = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_CONNECTTIMEOUT);
-        if (!StringUtils.isEmpty(connectionTime)) {
-            mongoDBProperties.setConnectTimeout(Integer.parseInt(connectionTime));
-        } else {
-            mongoDBProperties.setConnectTimeout(EnvironmentManager.MONGODB_DEFAULT_CONFIG_CONNECTTIMEOUT);
-        }
+        mongoDBProperties.setConnectTimeout(!StringUtils.isEmpty(connectionTime) ? Integer.parseInt(connectionTime) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_CONNECTTIMEOUT);
 
         String maxConnectionIdleTime = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_MAXCONNECTIONIDLETIME);
-        if (!StringUtils.isEmpty(maxConnectionIdleTime)) {
-            mongoDBProperties.setMaxConnectionIdleTime(Integer.parseInt(maxConnectionIdleTime));
-        } else {
-            mongoDBProperties.setMaxConnectionIdleTime(EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXCONNECTIONIDLETIME);
-        }
+        mongoDBProperties.setMaxConnectionIdleTime(!StringUtils.isEmpty(maxConnectionIdleTime) ? Integer.parseInt(maxConnectionIdleTime) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXCONNECTIONIDLETIME);
 
         String maxConnectionLifeTime = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_MAXCONNECTIONLIFETIME);
-        if (!StringUtils.isEmpty(maxConnectionLifeTime)) {
-            mongoDBProperties.setMaxConnectionLifeTime(Integer.parseInt(maxConnectionLifeTime));
-        } else {
-            mongoDBProperties.setMaxConnectionLifeTime(EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXCONNECTIONLIFETIME);
-        }
+        mongoDBProperties.setMaxConnectionLifeTime(!StringUtils.isEmpty(maxConnectionLifeTime) ? Integer.parseInt(maxConnectionLifeTime) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXCONNECTIONLIFETIME);
 
         String maxnWaitTime = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_MAXWAITTIME);
-        if (!StringUtils.isEmpty(maxnWaitTime)) {
-            mongoDBProperties.setMaxWaitTime(Integer.parseInt(maxnWaitTime));
-        } else {
-            mongoDBProperties.setMaxWaitTime(EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXWAITTIME);
-        }
+        mongoDBProperties.setMaxWaitTime(!StringUtils.isEmpty(maxnWaitTime) ? Integer.parseInt(maxnWaitTime) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXWAITTIME);
 
         String minConnectionsProHost = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_MINCONNECTIONSPERHOST);
-        if (!StringUtils.isEmpty(minConnectionsProHost)) {
-            mongoDBProperties.setMinConnectionsPerHost(Integer.parseInt(minConnectionsProHost));
-        } else {
-            mongoDBProperties.setMinConnectionsPerHost(EnvironmentManager.MONGODB_DEFAULT_CONFIG_MINCONNECTIONSPERHOST);
-        }
+        mongoDBProperties.setMinConnectionsPerHost(!StringUtils.isEmpty(minConnectionsProHost) ? Integer.parseInt(minConnectionsProHost) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_MINCONNECTIONSPERHOST);
 
         String serverSlectionTimeout = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_SERVERSELECTIONTIMEOUT);
-        if (!StringUtils.isEmpty(serverSlectionTimeout)) {
-            mongoDBProperties.setServerSelectionTimeout(Integer.parseInt(serverSlectionTimeout));
-        } else {
-            mongoDBProperties.setServerSelectionTimeout(EnvironmentManager.MONGODB_DEFAULT_CONFIG_SERVERSELECTIONTIMEOUT);
-        }
+        mongoDBProperties.setServerSelectionTimeout(!StringUtils.isEmpty(serverSlectionTimeout) ? Integer.parseInt(serverSlectionTimeout) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_SERVERSELECTIONTIMEOUT);
 
         String socketKeepAlive = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_SOCKETKEEPALIVE);
-        if (!StringUtils.isEmpty(socketKeepAlive)) {
-            mongoDBProperties.setSocketKeepAlive(Boolean.valueOf(socketKeepAlive));
-        } else {
-            mongoDBProperties.setSocketKeepAlive(EnvironmentManager.MONGODB_DEFAULT_CONFIG_SOCKETKEEPALIVE);
-        }
+        mongoDBProperties.setSocketKeepAlive(!StringUtils.isEmpty(socketKeepAlive) ? Boolean.valueOf(socketKeepAlive) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_SOCKETKEEPALIVE);
 
         String socketTimeout = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_SOCKETTIMEOUT);
-        if (!StringUtils.isEmpty(socketTimeout)) {
-            mongoDBProperties.setSocketTimeout(Integer.parseInt(socketTimeout));
-        } else {
-            mongoDBProperties.setSocketTimeout(EnvironmentManager.MONGODB_DEFAULT_CONFIG_SOCKETTIMEOUT);
-        }
+        mongoDBProperties.setSocketTimeout(!StringUtils.isEmpty(socketTimeout) ? Integer.parseInt(socketTimeout) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_SOCKETTIMEOUT);
 
         String threadsAllowedToBlockForConnectionMultiplier = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIG_THREADSALLOWEDTOBLOCKFORCONNECTIONMULTIPLIER);
-        if (!StringUtils.isEmpty(threadsAllowedToBlockForConnectionMultiplier)) {
-            mongoDBProperties.setThreadsAllowedToBlockForConnectionMultiplier(Integer.parseInt(threadsAllowedToBlockForConnectionMultiplier));
-        } else {
-            mongoDBProperties.setThreadsAllowedToBlockForConnectionMultiplier(EnvironmentManager.MONGODB_DEFAULT_CONFIG_THREADSALLOWEDTOBLOCKFORCONNECTIONMULTIPLIER);
-        }
+        mongoDBProperties.setThreadsAllowedToBlockForConnectionMultiplier(!StringUtils.isEmpty(threadsAllowedToBlockForConnectionMultiplier) ? Integer.parseInt(threadsAllowedToBlockForConnectionMultiplier) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_THREADSALLOWEDTOBLOCKFORCONNECTIONMULTIPLIER);
+
         return mongoDBProperties;
 
     }
 
     /**
-     * @Description 加载mongodb多数据源配置信息
-     * @Param [env]
-     * @Author mingj
-     * @Date 2019/4/14 18:19
-     * @Return com.extend.mongodb.properties.MongodbProperties
-     **/
+     * 加载数据源配置信息
+     *
+     * @param env   环境信息
+     * @param index 多数据源index
+     * @return {@link MongoDbProperties}
+     */
     public static MongoDbProperties loadMongoDbConfig(Environment env, int index) {
         String name = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_NAME, index);
         String authenticationDatabase = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_REPOSITORIESAUTHENTICATIONDATABASE, index);
@@ -139,86 +93,41 @@ public class AppConfigLoadUtil {
         mongoDBProperties.setMongodbUri(mongodbUri);
 
         String repositoriesEnabled = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_REPOSITORIESENABLED, index);
-        if (!StringUtils.isEmpty(repositoriesEnabled)) {
-            mongoDBProperties.setRepositoriesEnabled(Boolean.valueOf(repositoriesEnabled));
-        } else {
-            mongoDBProperties.setRepositoriesEnabled(EnvironmentManager.MONGODB_DEFAULT_CONFIG_REPOSITORIESENABLED);
-        }
+        mongoDBProperties.setRepositoriesEnabled(!StringUtils.isEmpty(repositoriesEnabled) ? Boolean.valueOf(repositoriesEnabled) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_REPOSITORIESENABLED);
 
         String repositoriesLocations = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_REPOSITORIESLOCATIONS, index);
-        if (!StringUtils.isEmpty(repositoriesLocations)) {
-            mongoDBProperties.setRepositoriesLocations(repositoriesLocations);
-        }
+        Optional.ofNullable(repositoriesLocations).ifPresent(location -> mongoDBProperties.setRepositoriesLocations(repositoriesLocations));
 
         String connectionPreHost = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_CONNECTIONSPERHOST, index);
-        if (!StringUtils.isEmpty(connectionPreHost)) {
-            mongoDBProperties.setConnectionsPerHost(Integer.parseInt(connectionPreHost));
-        } else {
-            mongoDBProperties.setConnectionsPerHost(EnvironmentManager.MONGODB_DEFAULT_CONFIG_CONNECTIONSPERHOST);
-        }
+        mongoDBProperties.setConnectionsPerHost(!StringUtils.isEmpty(connectionPreHost) ? Integer.parseInt(connectionPreHost) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_CONNECTIONSPERHOST);
 
         String connectionTime = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_CONNECTTIMEOUT, index);
-        if (!StringUtils.isEmpty(connectionTime)) {
-            mongoDBProperties.setConnectTimeout(Integer.parseInt(connectionTime));
-        } else {
-            mongoDBProperties.setConnectTimeout(EnvironmentManager.MONGODB_DEFAULT_CONFIG_CONNECTTIMEOUT);
-        }
+        mongoDBProperties.setConnectTimeout(!StringUtils.isEmpty(connectionTime) ? Integer.parseInt(connectionTime) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_CONNECTTIMEOUT);
 
         String maxConnectionIdleTime = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_MAXCONNECTIONIDLETIME, index);
-        if (!StringUtils.isEmpty(maxConnectionIdleTime)) {
-            mongoDBProperties.setMaxConnectionIdleTime(Integer.parseInt(maxConnectionIdleTime));
-        } else {
-            mongoDBProperties.setMaxConnectionIdleTime(EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXCONNECTIONIDLETIME);
-        }
+        mongoDBProperties.setMaxConnectionIdleTime(!StringUtils.isEmpty(maxConnectionIdleTime) ? Integer.parseInt(maxConnectionIdleTime) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXCONNECTIONIDLETIME);
 
         String maxConnectionLifeTime = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_MAXCONNECTIONLIFETIME, index);
-        if (!StringUtils.isEmpty(maxConnectionLifeTime)) {
-            mongoDBProperties.setMaxConnectionLifeTime(Integer.parseInt(maxConnectionLifeTime));
-        } else {
-            mongoDBProperties.setMaxConnectionLifeTime(EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXCONNECTIONLIFETIME);
-        }
+        mongoDBProperties.setMaxConnectionLifeTime(!StringUtils.isEmpty(maxConnectionLifeTime) ? Integer.parseInt(maxConnectionLifeTime) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXCONNECTIONLIFETIME);
 
         String maxnWaitTime = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_MAXWAITTIME, index);
-        if (!StringUtils.isEmpty(maxnWaitTime)) {
-            mongoDBProperties.setMaxWaitTime(Integer.parseInt(maxnWaitTime));
-        } else {
-            mongoDBProperties.setMaxWaitTime(EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXWAITTIME);
-        }
+        mongoDBProperties.setMaxWaitTime(!StringUtils.isEmpty(maxnWaitTime) ? Integer.parseInt(maxnWaitTime) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_MAXWAITTIME);
 
         String minConnectionsProHost = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_MINCONNECTIONSPERHOST, index);
-        if (!StringUtils.isEmpty(minConnectionsProHost)) {
-            mongoDBProperties.setMinConnectionsPerHost(Integer.parseInt(minConnectionsProHost));
-        } else {
-            mongoDBProperties.setMinConnectionsPerHost(EnvironmentManager.MONGODB_DEFAULT_CONFIG_MINCONNECTIONSPERHOST);
-        }
+        mongoDBProperties.setMinConnectionsPerHost(!StringUtils.isEmpty(minConnectionsProHost) ? Integer.parseInt(minConnectionsProHost) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_MINCONNECTIONSPERHOST);
 
         String serverSlectionTimeout = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_SERVERSELECTIONTIMEOUT, index);
-        if (!StringUtils.isEmpty(serverSlectionTimeout)) {
-            mongoDBProperties.setServerSelectionTimeout(Integer.parseInt(serverSlectionTimeout));
-        } else {
-            mongoDBProperties.setServerSelectionTimeout(EnvironmentManager.MONGODB_DEFAULT_CONFIG_SERVERSELECTIONTIMEOUT);
-        }
+        mongoDBProperties.setServerSelectionTimeout(!StringUtils.isEmpty(serverSlectionTimeout) ? Integer.parseInt(serverSlectionTimeout) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_SERVERSELECTIONTIMEOUT);
 
         String socketKeepAlive = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_SOCKETKEEPALIVE, index);
-        if (!StringUtils.isEmpty(socketKeepAlive)) {
-            mongoDBProperties.setSocketKeepAlive(Boolean.valueOf(socketKeepAlive));
-        } else {
-            mongoDBProperties.setSocketKeepAlive(EnvironmentManager.MONGODB_DEFAULT_CONFIG_SOCKETKEEPALIVE);
-        }
+        mongoDBProperties.setSocketKeepAlive(!StringUtils.isEmpty(socketKeepAlive) ? Boolean.valueOf(socketKeepAlive) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_SOCKETKEEPALIVE);
 
         String socketTimeout = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_SOCKETTIMEOUT, index);
-        if (!StringUtils.isEmpty(socketTimeout)) {
-            mongoDBProperties.setSocketTimeout(Integer.parseInt(socketTimeout));
-        } else {
-            mongoDBProperties.setSocketTimeout(EnvironmentManager.MONGODB_DEFAULT_CONFIG_SOCKETTIMEOUT);
-        }
+        mongoDBProperties.setSocketTimeout(!StringUtils.isEmpty(socketTimeout) ? Integer.parseInt(socketTimeout) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_SOCKETTIMEOUT);
 
         String threadsAllowedToBlockForConnectionMultiplier = ConfigurationLoadUtil.getProperty(env, EnvironmentManager.MONGODB_CONFIGS_THREADSALLOWEDTOBLOCKFORCONNECTIONMULTIPLIER, index);
-        if (!StringUtils.isEmpty(threadsAllowedToBlockForConnectionMultiplier)) {
-            mongoDBProperties.setThreadsAllowedToBlockForConnectionMultiplier(Integer.parseInt(threadsAllowedToBlockForConnectionMultiplier));
-        } else {
-            mongoDBProperties.setThreadsAllowedToBlockForConnectionMultiplier(EnvironmentManager.MONGODB_DEFAULT_CONFIG_THREADSALLOWEDTOBLOCKFORCONNECTIONMULTIPLIER);
-        }
+        mongoDBProperties.setThreadsAllowedToBlockForConnectionMultiplier(!StringUtils.isEmpty(threadsAllowedToBlockForConnectionMultiplier) ? Integer.parseInt(threadsAllowedToBlockForConnectionMultiplier) : EnvironmentManager.MONGODB_DEFAULT_CONFIG_THREADSALLOWEDTOBLOCKFORCONNECTIONMULTIPLIER);
+
         return mongoDBProperties;
     }
 }
