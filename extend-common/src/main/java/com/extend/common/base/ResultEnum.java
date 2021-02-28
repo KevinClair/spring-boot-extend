b890d3328d1941f53250aab8eabc5bc4a652414d
KevinClair <704714211@qq.com>
2021-02-28 17:26:20 +0800

    Add enum type.

--- extend-common/src/main/java/com/extend/common/base/ResultEnum.java
+++ extend-common/src/main/java/com/extend/common/base/ResultEnum.java
@@ -8,7 +8,11 @@ package com.extend.common.base;
 public enum ResultEnum {
 
     REQUEST_SUCCESS("200", "请求或操作成功", true),
-    REQUEST_SUCCESS_WITHOUTDATA("201", "请求成功但是查无数据", true)
+    REQUEST_SUCCESS_WITHOUTDATA("201", "请求成功但是查无数据", true),
+    ACCESS_DENIED("599", "Access denied", false),
+    PARAM_ERROR("598", "Param check error", false),
+    PARAM_BIND_ERROR("597", "Param bind error", false),
+    SYSTEM_ERROR("500", "System error", false)
     ;
 
     private String code;
