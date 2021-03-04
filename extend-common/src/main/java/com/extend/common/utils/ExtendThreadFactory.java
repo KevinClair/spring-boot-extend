6d7550b3024eb78a1d57521dfcbda1c50af14540
KevinClair <704714211@qq.com>
2021-03-04 23:17:37 +0800

    添加ThreadFactory线程池工厂；

---
+++ extend-common/src/main/java/com/extend/common/utils/ExtendThreadFactory.java
@@ -0,0 +1,40 @@
+package com.extend.common.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 线程池工厂
+ *
+ * @author KevinClair
+ */
+public class ExtendThreadFactory implements ThreadFactory {
+
+
+    private static final AtomicInteger poolNumber = new AtomicInteger(1);
+    private final ThreadGroup group;
+
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String namePrefix;
+
+    ExtendThreadFactory(String name) {
+
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+        if (StringUtils.isBlank(name)) {
+            name = "pool";
+        }
+        namePrefix = "extend-works-" + poolNumber.getAndIncrement() + "-thread-" + name + "-";
+    }
+
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+        if (t.isDaemon())
+            t.setDaemon(false);
+        if (t.getPriority() != Thread.NORM_PRIORITY)
+            t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
+}
