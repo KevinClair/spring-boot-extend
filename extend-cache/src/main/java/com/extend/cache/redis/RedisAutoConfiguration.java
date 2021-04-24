a71551b23455764d210ee7bea4c06dbeb3d885b2
KevinClair <704714211@qq.com>
2021-04-24 18:47:39 +0800

    Add bean configuration.

--- extend-cache/src/main/java/com/extend/cache/redis/RedisAutoConfiguration.java
+++ extend-cache/src/main/java/com/extend/cache/redis/RedisAutoConfiguration.java
@@ -1,20 +1,24 @@
 package com.extend.cache.redis;
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import com.extend.cache.serializer.FastJsonRedisSerializer;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.io.Serializable;
+import java.time.Duration;
 
 /**
  * Redis配置注入
@@ -23,30 +27,92 @@ import java.io.Serializable;
  **/
 @Configuration
 @EnableCaching
-@AutoConfigureAfter(org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class)
+@AutoConfigureBefore(org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class)
 public class RedisAutoConfiguration {
 
+    private ConfigurableEnvironment environment;
+
+    public RedisAutoConfiguration(ConfigurableEnvironment environment) {
+        this.environment = environment;
+    }
+
     /**
-     * 默认情况下的模板只能支持RedisTemplate<String, String>，也就是只能存入字符串，因此支持序列化
+     * 注册RedisTemplate
+     *
+     * @param redisConnectionFactory redis连接工厂
+     * @return {@link RedisTemplate}
      */
     @Bean
-    public RedisTemplate<String, Serializable> redisCacheTemplate(LettuceConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, Serializable> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Serializable> template = new RedisTemplate<>();
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setConnectionFactory(redisConnectionFactory);
+        FastJsonRedisSerializer<Object> redisSerializer = new FastJsonRedisSerializer<>(Object.class);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(redisSerializer);
+
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(redisSerializer);
+
+        template.afterPropertiesSet();
+        // 关闭事务
+        template.setEnableTransactionSupport(false);
         return template;
     }
 
     /**
-     * 配置使用注解的时候缓存配置，默认是序列化反序列化的形式，加上此配置则为 json 形式
+     * 注册StringRedisTemplate
+     *
+     * @param redisConnectionFactory redis连接工厂
+     * @return {@link StringRedisTemplate}
+     */
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory){
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+        stringRedisTemplate.setEnableTransactionSupport(false);
+        return stringRedisTemplate;
+    }
+
+    /**
+     * 缓存管理器
+     *
+     * @param factory redis连接工厂
+     * @return {@link CacheManager}
      */
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory factory) {
         // 配置序列化
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
-        RedisCacheConfiguration redisCacheConfiguration = config.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())).serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(1)).computePrefixWith(cacheKeyPrefix());
+        return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(factory)).cacheDefaults(config).build();
+    }
 
-        return RedisCacheManager.builder(factory).cacheDefaults(redisCacheConfiguration).build();
+    /**
+     * 缓存key生成器
+     *
+     * @return {@link KeyGenerator}
+     */
+    @Bean
+    public KeyGenerator keyGenerator(){
+        return  (o, method, objects) -> {
+          StringBuilder stringBuilder = new StringBuilder();
+          stringBuilder.append(o.getClass().getName()).append("#").append(method.getName()).append("@");
+            for (Object object : objects) {
+                stringBuilder.append(object.toString());
+            }
+            return stringBuilder.toString();
+        };
+    }
+
+    /**
+     * 缓存Key，重置生成redis缓存时的目录结构
+     *
+     * @return
+     */
+    @Bean
+    public CacheKeyPrefix cacheKeyPrefix(){
+        return cacheName -> {
+          StringBuilder cachekey = new StringBuilder();
+          return cachekey.append(environment.getProperty("spring.application.name")).append(":").append(cacheName).append(":").toString();
+        };
     }
 }
